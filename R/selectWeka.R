81b66e43364ea46b0e70adf2ffd0b4176c4e384a
Dan Tenenbaum <dtenenba@fhcrc.org>
2016-04-06 18:59:34 +0000

    finish workaround
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BioSeqClass@115912 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/selectWeka.R
+++ R/selectWeka.R
@@ -1,32 +1,32 @@
 selectWeka <- function(train,evaluator="CfsSubsetEval",search="BestFirst",n){
-## 
-  
+##
+
 ## evaluator:
-## "CfsSubsetEval": Evaluates the worth of a subset of attributes by considering 
-##                  the individual predictive ability of each feature along with 
-##                  the degree of redundancy between them. Subsets of features 
-##                  that are highly correlated with the class while having low 
-##                  intercorrelation are preferred. 
+## "CfsSubsetEval": Evaluates the worth of a subset of attributes by considering
+##                  the individual predictive ability of each feature along with
+##                  the degree of redundancy between them. Subsets of features
+##                  that are highly correlated with the class while having low
+##                  intercorrelation are preferred.
 ## "InfoGainAttributeEval":
 ## "SVMAttributeEval"
 
 ## search:
-## "BestFirst": Searches the space of attribute subsets by greedy hillclimbing 
+## "BestFirst": Searches the space of attribute subsets by greedy hillclimbing
 ##              augmented with a backtracking facility.
 ## "Ranker"
 
 ## n: the number of selected attributes.
-## 
+##
   trainFile = tempfile("tempTrain")
   #require(foreign)
   write.arff(train,trainFile)
   tmpOut = tempfile("tempOut")
   if(search=="Ranker"){
-      command = paste("java -cp ", file.path(path.package("BioSeqClass"), "scripts", "weka.jar"), 
+      command = paste(dQuote(.javaExecutable()), "-cp ", file.path(path.package("BioSeqClass"), "scripts", "weka.jar"),
                       " weka.attributeSelection.", evaluator, " -i ", trainFile,
-                      " -s \"weka.attributeSelection.", search, " -N ", n, "\"", sep="" )  
+                      " -s \"weka.attributeSelection.", search, " -N ", n, "\"", sep="" )
   }else{
-      command = paste("java -cp ", file.path(path.package("BioSeqClass"), "scripts", "weka.jar"),
+      command = paste(dQuote(.javaExecutable()), "-cp ", file.path(path.package("BioSeqClass"), "scripts", "weka.jar"),
                       " weka.attributeSelection.", evaluator, " -i ", trainFile,
                       " -s weka.attributeSelection.", search, sep="" )
   }
@@ -35,5 +35,5 @@ selectWeka <- function(train,evaluator="CfsSubsetEval",search="BestFirst",n){
   tmp <- sub(" :.*","",sub("Selected attributes: ","",tmp))
   feature <- as.numeric(unlist(strsplit(tmp,split=",")))
   unlink(c(trainFile,tmpOut))
-  feature 
+  feature
 }
