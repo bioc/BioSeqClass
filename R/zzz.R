56cb2d5c28c2d76367f87c5038cb799e1ee73485
Dan Tenenbaum <dtenenba@fhcrc.org>
2016-04-06 18:52:52 +0000

    work around java path bug
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BioSeqClass@115910 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,25 @@
+
+# from http://stackoverflow.com/a/34031214/470769
+Sys.which2 <- function(cmd) {
+    stopifnot(length(cmd) == 1)
+    if (.Platform$OS.type == "windows") {
+        suppressWarnings({
+            pathname <- shell(sprintf("where %s 2> NUL", cmd), intern=TRUE)[1]
+        })
+        if (!is.na(pathname)) return(stats::setNames(pathname, cmd))
+    }
+    Sys.which(cmd)
+}
+
+.javaExecutable <- function() Sys.which2("java")
+
+
+
 .onLoad <- function(libname, pkgname) {
   # add vigette to menu; support Windows only
   if(.Platform$OS.type == "windows" && interactive()
       && .Platform$GUI == "Rgui"){
           addVigs2WinMenu("BioSeqClass")
-  } 
+  }
   data("dssp.ss", "aa.index", "PROPERTY", "DiProDB", package="BioSeqClass")
 }
