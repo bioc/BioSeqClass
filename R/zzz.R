dcf5f746948427e5d3d704a6fcb8053c0debc3ab
lshep <lori.shepherd@roswellpark.org>
2020-06-08 10:03:34 -0400

    Deprecate
     - unresponsive

--- R/zzz.R
+++ R/zzz.R
@@ -23,3 +23,9 @@ Sys.which2 <- function(cmd) {
   }
   data("dssp.ss", "aa.index", "PROPERTY", "DiProDB", package="BioSeqClass")
 }
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.13")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
